a22febdb5d001f414eb3257d70ec4dad7fca6319
rohit4242 <rohitluni4242@gmail.com>
2023-09-10 15:54:16 +0530

    init

--- server/example/views.py
+++ server/example/views.py
@@ -1,12 +1,5 @@
-
 from django.http import HttpResponse
 
-def index():
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Chotila.")
--- server/server/urls.py
+++ server/server/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("example.views")),
+    path("", include("example.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("brief.urls")),
 ]

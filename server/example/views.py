96922a2262480c1b5b703c752931d6f3798226fa
rohit4242 <rohitluni4242@gmail.com>
2023-09-10 18:41:15 +0530

    init

--- server/example/views.py
+++ server/example/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Chotila.")
+    return HttpResponse("Hello, world.From Rohit Luni. You're at the Chotila.")
